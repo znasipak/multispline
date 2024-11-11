4fd16238b3f6edc88ca351f584fd493845aec5e6
Zach Nasipak <13366660+znasipak@users.noreply.github.com>
2024-11-11 14:32:48 +0000

    Initial commit with rudimentary example of BicubicSpline class

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # multispline
 A Python package for generating cubic splines in multiple dimensions
+
+# Installation
+
+`multispline` relies on a few dependencies to install and run, namely
+a C/C++ compiler (e.g., `g++`), `Cython`, `numpy`, and `python >= 3.7`, though we recommend using at least Python 3.9.
+To reduce package conflicts and ensure that the proper dependencies are installed,
+we recommend using Anaconda and its virtual environments.
+
+Create a conda environment `spline-env` (or whatever name you would like)
+with the necessary dependencies to install `multispline`. For MACOSX Intel run:
+```
+conda create -n spline-env -c conda-forge Cython numpy clang_osx-64 clangxx_osx-64 python=3.9
+conda activate spline-env
+```
+This may also work for MACOSX silicon, though alternatively one should use:
+For MACOSX Intel run:
+```
+conda create -n spline-env -c conda-forge Cython numpy clang_osx-arm64 clangxx_osx-arm64 python=3.9
+conda activate spline-env
+```
+See Troubleshooting.
+To instead include the necessary compiler on linux run:
+```
+conda create -n spline-env -c conda-forge Cython numpy gcc_linux-64 gxx_linux-64 python=3.9
+conda activate spline-env
+```
+Next clone the :code:`multispline` repository from GitHub:
+```
+git clone https://github.com/znasipak/multspline.git
+cd multispline
+```
+Finally, we recommend installing the package via `pip`:
+```
+pip install .
+```
+
+# Conda Environments with Jupyter
+
+To run the code in a jupyter notebook, we recommend installing
+the following dependencies through your preferred package manager,
+such as `conda`: 
+```
+conda install ipykernel matplotlib
+```
+One can then make the environment accessible within Jupyter by
+running
+```
+python -m ipykernel install --user --name=multispline-env
+```
---
+++ setup.py
@@ -0,0 +1,71 @@
+import os
+from Cython.Build import cythonize
+from setuptools import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+import sys
+
+
+"""
+For some reason, the libraries extension does not support extra compiler arguments. So
+instead, we pass them directly by changing the evironment variable CFLAGS
+"""
+compiler_flags = ["-std=c++11", "-fopenmp", "-march=native"]
+libraries = []
+
+# set some flags and libraries depending on the system platform
+if sys.platform.startswith('win32'):
+    compiler_flags.append('/Od')
+    libraries.append('gomp')
+elif sys.platform.startswith('darwin'):
+    compiler_flags.append('-O2')
+    libraries.append('omp')
+elif sys.platform.startswith('linux'):
+    compiler_flags.append('-O2')
+    libraries.append('gomp')
+    
+
+CFLAGS = os.getenv("CFLAGS")
+if CFLAGS is None:
+    CFLAGS = ""
+else:
+    CFLAGS = str(CFLAGS)
+os.environ["CFLAGS"] = CFLAGS
+os.environ["CFLAGS"] += " "
+os.environ["CFLAGS"] += ' '.join(compiler_flags)
+base_path = sys.prefix
+
+full_dependence = ["cpp/src/spline.cpp"]
+
+cpu_extension = dict(
+    libraries=libraries,
+    language='c++',
+    include_dirs=["cpp/include", np.get_include(), base_path + "/include"],
+)
+
+wave_ext = Extension(
+    "splinecy", 
+    sources=["cython/spline_wrap.pyx", *set(full_dependence)], 
+    **cpu_extension,
+)
+
+ext_modules = [wave_ext]
+
+setup(
+    name = "multispline",
+    author = "Zach Nasipak",
+    version = "0.1.0",
+    description = "Cubic splines in multiple dimensions",
+    ext_modules = cythonize(ext_modules, language_level = "3"),
+    py_modules = ["multispline.spline"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Programming Language :: C++",
+        "Programming Language :: Cython",
+    ],
+    cmdclass = {'build_ext': build_ext},
+    zip_safe = False
+)
